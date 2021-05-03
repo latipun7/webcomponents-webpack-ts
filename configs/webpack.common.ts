fcae530c4828834430b663fa2cb10780c2b7bc7e
Latif Sulistyo <latifsulistyo.me@gmail.com>
2021-05-03 16:52:14 +0700

    feat: use webpack plugin to generate webmanifest 🧩

--- configs/webpack.common.ts
+++ configs/webpack.common.ts
@@ -1,6 +1,7 @@
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import type { Configuration } from 'webpack';
 
@@ -35,6 +36,21 @@ const common: Configuration = {
       inject: 'head',
       scriptLoading: 'defer',
       cache: false,
+    }),
+    new FaviconsWebpackPlugin({
+      logo: resolvePath('src', 'assets', 'logo-icons.png'),
+      prefix: 'assets/',
+      inject: true,
+      manifest: resolvePath('src', 'assets', 'app.webmanifest'),
+      favicons: {
+        appName: 'BrandTitle',
+        appShortName: 'BrandTitle',
+        appDescription: 'Description of the BrandTitle',
+        theme_color: '#009688',
+        background: '#36393f',
+        start_url: '/',
+        display: 'standalone',
+      },
     }),
   ],
   module: {
